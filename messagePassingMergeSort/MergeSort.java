41393939e6bfa2917c7ef5e3249d69e0008593dd
Anders Fischer <andersfischern@me.com>
2018-12-18 10:15:41 +0100

    Initial implementation of MergeSort in Akka.

---
+++ messagePassingMergeSort/MergeSort.java
@@ -0,0 +1,134 @@
+
+//Compile & run:
+// javac -cp ../libraries/scala.jar:../libraries/akka-actor.jar MergeSort.java
+// java -cp ../libraries/scala.jar:../libraries/akka-actor.jar:../libraries/akka-config.jar:. MergeSort
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import akka.actor.*;
+
+class MergeSort {
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("MergeSortPipelineSystem");
+
+        final ActorRef tester = system.actorOf(Props.create(TesterActor.class));
+        final ActorRef sorter = system.actorOf(Props.create(SorterActor.class));
+        tester.tell(new InitMessage(sorter), ActorRef.noSender());
+    }
+}
+
+class SorterActor extends UntypedActor {
+    public void onReceive(Object o) throws Exception {
+        if (o instanceof SortMessage) {
+            var list = ((SortMessage) o).list;
+            var x = ((SortMessage) o).receiver;
+
+            if (list.size() > 1) {
+                var m = getContext().actorOf(Props.create(MergerActor.class));
+                m.tell(new ResultMessage(x), ActorRef.noSender());
+
+                var l1 = list.subList(0, list.size() / 2);
+                var l2 = list.subList(list.size() / 2, list.size());
+
+                var s1 = getContext().actorOf(Props.create(SorterActor.class));
+                s1.tell(new SortMessage(l1, m), ActorRef.noSender());
+
+                var s2 = getContext().actorOf(Props.create(SorterActor.class));
+                s2.tell(new SortMessage(l2, m), ActorRef.noSender());
+            }
+
+            else {
+                x.tell(new SortedMessage(list), ActorRef.noSender());
+            }
+        }
+    }
+}
+
+class MergerActor extends UntypedActor {
+    private ActorRef receiver;
+    private List<Integer> l1;
+    private List<Integer> l2;
+
+    private List<Integer> merge(List<Integer> l1, List<Integer> l2) {
+        var result = new ArrayList<Integer>();
+        result.addAll(l1);
+        result.addAll(l2);
+        result.sort((a, b) -> a.compareTo(b));
+        return result;
+    }
+
+    public void onReceive(Object o) throws Exception {
+        if (o instanceof ResultMessage) {
+            this.receiver = ((ResultMessage) o).receiver;
+        }
+
+        // TODO:
+        else if (o instanceof SortedMessage) {
+            // Since we can't do nested onReceive, we do stateful receiver and
+            // list building on this actor.
+            if (receiver == null)
+                return;
+            if (l1 == null)
+                l1 = ((SortedMessage) o).sorted;
+            if (l1 != null)
+                l2 = ((SortedMessage) o).sorted;
+            var sorted = merge(l1, l2);
+            System.out.println("Merged: " + sorted);
+            receiver.tell(new SortedMessage(sorted), ActorRef.noSender());
+        }
+    }
+}
+
+class TesterActor extends UntypedActor {
+    public void onReceive(Object o) throws Exception {
+        if (o instanceof InitMessage) {
+            var sorter = ((InitMessage) o).sorter;
+            var list = Arrays.asList(new Integer[] { 8, 7, 6, 5, 4, 3, 2, 1 });
+            sorter.tell(new SortMessage(list, getSelf()), ActorRef.noSender());
+        } else if (o instanceof SortedMessage) {
+            System.out.println("RESULT: " + ((SortedMessage) o).sorted);
+        }
+    }
+}
+
+class InitMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public final ActorRef sorter;
+
+    public InitMessage(ActorRef sorter) {
+        this.sorter = sorter;
+    }
+}
+
+class SortMessage implements Serializable {
+    private static final long serialVersionUID = 2L;
+    public final List<Integer> list;
+    public final ActorRef receiver;
+
+    public SortMessage(List<Integer> list, ActorRef receiver) {
+        this.list = list;
+        this.receiver = receiver;
+    }
+}
+
+class ResultMessage implements Serializable {
+    private static final long serialVersionUID = 3L;
+    public final ActorRef receiver;
+
+    public ResultMessage(ActorRef receiver) {
+        this.receiver = receiver;
+    }
+}
+
+class SortedMessage implements Serializable {
+    private static final long serialVersionUID = 4L;
+    public final List<Integer> sorted;
+
+    public SortedMessage(List<Integer> sorted) {
+        this.sorted = sorted;
+    }
+}
